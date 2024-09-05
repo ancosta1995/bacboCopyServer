405af2cbff84b450597a2af644d090a36cd1e172
Anderson Costa <ancosta1995@gmail.com>
2024-09-05 05:49:58 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const PORT = 3000;
+const io = require('socket.io')(PORT, {
+    cors: {
+        origin: "*"
+    }
+});
+
+const clients = new Set();
+const token = 12345678;
+
+io.on('connection', (socket) => {
+    console.log('Uma nova extensão conectou.');
+    clients.add(socket);
+
+    socket.on('receiveBet', (data) => {
+        if (data.token === token) { // Se o token for válido, propaga a mensagem para todos os outros clientes
+            console.log(`Dados recebido de master: ${JSON.stringify(data)}`);
+
+            socket.emit('receiveBetConfirmation', { status: true });
+
+            socket.broadcast.emit('sendBet', data.bet);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Uma extensão se desconectou.');
+        clients.delete(socket);
+    });
+});
+
+console.log(`Servidor Socket.IO rodando na porta ${PORT}.`);
